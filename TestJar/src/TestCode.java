64ce4dec5e4307c6fdc7412f3d17aba474172b7c
huangrob <robert.huang2000@gmail.com>
2016-08-30 08:28:45 -0700

    TestCode for Jar File

---
+++ TestJar/src/TestCode.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Robert Huang
+ * Date: August 30, 2016
+ * TestCode program -- used to show how to create a jar file that I can turn in to Classroom
+ */
+public class TestCode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
