da68ddccb414391e51843daf1f7c1cad16d9243a
huangrob <robert.huang2000@gmail.com>
2016-09-02 08:51:17 -0700

    Math....

--- Calculate Library/Calculate.java
+++ Calculate Library/Calculate.java
@@ -1,16 +1,57 @@
 
 public class Calculate {
 
-	public static double square(double i) {
+	public static double square(double s) {
 		double x;
-		x=i*i;
+		x=s*s;
 		return x;
 	}
+		
 	public static double cube(double c) {
 		double x;
 		x=c*c*c;
 		return x;
 	}
+		
+	public static double average(double x, double y) {
+		double a =(x+y)/2;
+		return a;	
+		
+	public static double avera(double i, double j, double k) {
+		return (i+j+k)/3;
+	}
+	
+	public static double toDegrees(double rad) {
+		double pi = 3.14159;
+		return rad/pi * 180;
+			
+	}
+	
+	public static double toRadians(double deg) {
+		double pi = 3.14159;
+		return deg/180 * pi;
+	}
+	
+	public static double discriminant(double a, double b, double c){
+		return b*b + 4*a*c;
+	}
+	
+	public static String toImproperFrac(int a, int b, int c){
+		return (a * c + b) + "/" + c;
+		
+	}
+	
+	public static String toMixedNum(int a, int b){
+		return 
+	}
+	
+	
+	
+	
+	
+	
+	
 
-
+	}
+	
 }
