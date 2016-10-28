21f8d7f7638a46791a543d9fb3e7711fec48e4ee
stephaniekwan <sttefy.kwan@gmail.com>
2016-10-28 08:47:08 -0700

    kjlhhjkjkjjhh

---
+++ Array Mini Lab/src/ArrayMiniLab.java
@@ -0,0 +1,17 @@
+
+public class ArrayMiniLab {
+
+	public static void main (String[] args){
+		int num = 7;
+		String strMain = "APCS";
+		int[] arrMain = {1,2,3,4,5};
+		
+	}
+	
+	public static void changeMe(int x, String str, int[] arr){
+		System.out.println(x);
+		System.out.println(str);
+		System.out.println(x);
+	}
+	
+}
